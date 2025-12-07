40a597e4f0eb8cd8d2d10f2be148411692874866
Bhawana0218 <bhawanabishtbhanu1205@gmail.com>
2025-12-07 13:52:12 +0530

    Change in URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Menu from './Pages/Menu';
 import Footer from './Footer';
 import Home from './Pages/Home';
